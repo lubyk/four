2cb1d5fc08e606fcac5326e80db6d53871549a57
Daniel Bünzli <daniel.buenzli@erratique.ch>
2013-01-28 16:35:54 +0100

    Fix V2 metatable.

--- lib/four/V2.lua
+++ lib/four/V2.lua
@@ -12,8 +12,6 @@ setmetatable(lib, { __call = function(lib, ...) return lib.V2(...) end })
 
 -- h2. Constructor and accessors
 
-local meta = {} -- for operators, see at the end of file
-
 --[[--
   @V2(x, y)@ is a vector with the corresponding components.
 
@@ -29,7 +27,7 @@ function lib.V2(x, y)
       assert(false, string.format("Cannot convert %s to %s", x.type, lib.type))
     end
   end
-  setmetatable(v, meta)
+  setmetatable(v, lib)
   return v
 end
 
@@ -208,10 +206,10 @@ end
 
 -- h2. Operators
 
-meta.__unm = lib.neg
-meta.__add = lib.add
-meta.__sub = lib.sub
-meta.__mul = lib.smul
-meta.__tostring = lib.tostring
+lib.__unm = lib.neg
+lib.__add = lib.add
+lib.__sub = lib.sub
+lib.__mul = lib.smul
+lib.__tostring = lib.tostring
 
 
