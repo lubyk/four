b3f71aadb892c38a13e5deda9343f88db3633b97
Daniel Bünzli <daniel.buenzli@erratique.ch>
2013-01-31 18:08:16 +0100

    Rename tricolor.lua to minimal.lua.

--- test/minimal.lua
+++ test/minimal.lua
@@ -1,29 +1,32 @@
 -- Draws a tri-colored triangle.
 
 require 'lubyk'
+
 local V2 = four.V2
+local Buffer = four.Buffer 
+local Geometry = four.Geometry
 local Effect = four.Effect 
 
-function triangle () -- A triangle inside clip space
-  local vs = four.Buffer { dim = 3, scalar_type = four.Buffer.FLOAT } 
-  local cs = four.Buffer { dim = 4, scalar_type = four.Buffer.FLOAT }
-  local is = four.Buffer { dim = 1, scalar_type = four.Buffer.UNSIGNED_INT }
+function triangle () -- Geometry object for a triangle inside clip space
+  local vs = Buffer { dim = 3, scalar_type = Buffer.FLOAT } 
+  local cs = Buffer { dim = 4, scalar_type = Buffer.FLOAT }
+  local is = Buffer { dim = 1, scalar_type = Buffer.UNSIGNED_INT }
   
   vs:push3D(-0.8, -0.8, 0.0)      -- Vertices
   vs:push3D( 0.8, -0.8, 0.0)
   vs:push3D( 0.0,  0.8, 0.0)
 
-  cs:pushV4(four.Color.red ())    -- Colors
+  cs:pushV4(four.Color.red ())    -- Vertices' colors
   cs:pushV4(four.Color.green ())
   cs:pushV4(four.Color.blue ())
 
   is:push3D(0, 1, 2)              -- Index for a single triangle
 
-  return four.Geometry { primitive = four.Geometry.TRIANGLE, 
-                         index = is, data = { vertex = vs, color = cs}}
+  return Geometry { primitive = Geometry.TRIANGLE, 
+                    index = is, data = { vertex = vs, color = cs}}
 end
 
-local effect = Effect
+local effect = Effect -- Colors the triangle
 {
   vertex = Effect.Shader [[
     in vec4 vertex;
@@ -43,19 +46,20 @@ local effect = Effect
   ]]
 }
 
-local obj = { geometry = triangle (), effect = effect }
+local obj = { geometry = triangle (), effect = effect } -- renderable
 local camera = four.Camera()
 
 -- Render
 
 local w, h = 600, 400
 local renderer = four.Renderer { size = V2(w, h) }
+
 local win = mimas.GLWindow()
 function win:resizeGL(w, h) renderer.size = V2(w, h) end
 function win:paintGL() renderer:render(camera, {obj}) end
 function win:initializeGL() renderer:logInfo() end  
 
-win:move(800, 50)
+win:move(100, 100)
 win:resize(w, h)
 win:show ()
 run ()
